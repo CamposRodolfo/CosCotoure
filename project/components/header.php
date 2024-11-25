2f5b1ed404ab8d58264219d4cab666e2efd9dd3f
Rodolfo Campos <SrRodolfoCampos@gmail.com>
2024-11-25 05:21:59 -0500

    v1.6.0 (fix header)

--- project/components/header.php
+++ project/components/header.php
@@ -26,75 +26,67 @@ $total_cart_items = $count_cart_items->rowCount(); // Cuenta el número de filas
 </head>
 <body>
     <header class="header">
-        <section calss="flex">
-            <a href="index.php">CosCotoure</a>
+        <section class="flex">
+            <a href="index.php" class="logo">CosCotoure</a>
 
             <nav class="navbar">
-                <a href="index.php">Ver Productos</a>
-                <a href="agregar_producto.php">Agregar Producto</a>
-                <a href="lista_pedidos.php">Lista de Pedidos</a>
+                <a href="agregar_producto.php">Añadir producto</a>
+                <a href="index.php">Ver productos</a>
+                <a href="lista_pedidos.php">Mis pedidos</a>
 
-                <a href="shopping_cart.php" class="cart-btn">
-                    Carro de Compras <span><?= $total_cart_items ?? 0; ?></span>
-                </a>
+                <a href="carro_compras.php" class="cart-btn">Carro de Compras<span><?= $total_cart_items ?? 0; ?></span></a>
 
-                <!-- <span class="navigation__group">
-                    <button type="button" class="profile__btn">
-                        <img  src="../../project_assets/header/Usuario.png" alt="Usuario">
-                    </button>
-                </span>  -->     
             </nav>
         </section>
+
         <span class="navigation__group">
-            <button type="button" class="profile__btn">
-                <img  src="../../project_assets/header/Usuario.png" alt="Usuario">
-            </button>
-        </span>
-        <!-- Dropdown Menu -->
-        <div class="dropdown__wrapper hide dropdown__wrapper--fade-in none" id="dropdown-menu">
-            <nav>
-                <div class="profile__wrapper">
-                    <img class="profile__avatar" src="../../project_assets/header/Usuario.png" alt="<?php echo htmlspecialchars($nombreUsuario); ?>">
-                    <div class="profile__info">
-                        <h2 class="profile__name"><?php echo htmlspecialchars($nombreUsuario); ?></h2>
-                        <p class="profile__email"><?php echo htmlspecialchars($correoUsuario); ?></p>
+                <button type="button" class="profile__btn">
+                    <img  src="../../project_assets/header/Usuario.png" alt="Usuario">
+                </button>
+            </span>
+            <!-- Dropdown Menu -->
+            <div class="dropdown__wrapper hide dropdown__wrapper--fade-in none" id="dropdown-menu">
+                <nav>
+                    <div class="profile__wrapper">
+                        <img class="profile__avatar" src="../../project_assets/header/Usuario.png" alt="<?php echo htmlspecialchars($nombreUsuario); ?>">
+                        <div class="profile__info">
+                            <h2 class="profile__name"><?php echo htmlspecialchars($nombreUsuario); ?></h2>
+                            <p class="profile__email"><?php echo htmlspecialchars($correoUsuario); ?></p>
+                        </div>
                     </div>
-                </div>
-                <hr class="divider">
-                <ul>
-                    <li>
-                        <a href="profile.php">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-user-check">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
-                                <path d="M6 21v-2a4 4 0 0 1 4 -4h4" />
-                                <path d="M15 19l2 2l4 -4" />
-                            </svg>    
-                            Mi Cuenta
-                        </a>
-                    </li>
-                </ul>
-                <hr class="divider">
-                <ul>
-                    <li>
-                        <form action="" method="POST">
-                            <button type="submit" name="logout" style="background: none; border: none; display: flex; align-items: center;">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-logout">
+                    <hr class="divider">
+                    <ul>
+                        <li>
+                            <a href="profile.php">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-user-check">
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                    <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
-                                    <path d="M9 12h12l-3 -3" />
-                                    <path d="M18 15l3 -3" />
-                                </svg>
-                                Cerrar Sesión
-                            </button>
-                        </form>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-
+                                    <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+                                    <path d="M6 21v-2a4 4 0 0 1 4 -4h4" />
+                                    <path d="M15 19l2 2l4 -4" />
+                                </svg>    
+                                Mi Cuenta
+                            </a>
+                        </li>
+                    </ul>
+                    <hr class="divider">
+                    <ul>
+                        <li>
+                            <form action="" method="POST">
+                                <button type="submit" name="logout" style="background: none; border: none; display: flex; align-items: center;">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-logout">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                        <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+                                        <path d="M9 12h12l-3 -3" />
+                                        <path d="M18 15l3 -3" />
+                                    </svg>
+                                    Cerrar Sesión
+                                </button>
+                            </form>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
     </header>
-
     <script src="../assets/js/header.js"></script>
 </body>
 </html>
